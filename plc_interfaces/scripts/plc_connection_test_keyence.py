4304f645bdc084a88e90ee3aad0e11ad24510819
Yuki Asano <yasano@g.ecc.u-tokyo.ac.jp>
2024-02-01 14:47:20 +0900

    add connection test

--- plc_interfaces/scripts/plc_connection_test_keyence.py
+++ plc_interfaces/scripts/plc_connection_test_keyence.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import time
 from plc_interfaces.plc_interface_keyence import PLCInterfaceKeyence
 
-plc = PLCInterfaceKeyence()
-plc.open('192.168.0.10')
+
+plc = PLCInterfaceKeyence('192.168.0.10')
+plc.open()
